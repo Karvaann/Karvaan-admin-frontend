9283e6565b38d57fc487ba31b3113b1e843fe130
Samarth10 <samarth.saxena2002@gmail.com>
2025-07-17 12:22:29 +0530

    Working branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
-import DashBoard from "./pages/DashBoard";
+import MyBookings from "./pages/MyBookings";
 import "./App.css";
 import Leads from "./pages/Leads";
 import Tasks from "./pages/Tasks";
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/" element={<DashBoard />} />
+          <Route path="/" element={<MyBookings />} />
           <Route path="/leads" element={<Leads />} />
           <Route path="/tasks" element={<Tasks />} />
         </Routes>
